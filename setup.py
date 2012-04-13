ebfc1912f8aacb252295ee4e00535ae3faaa572f
Andy Blackburn <andyblac@o2.co.uk>
2012-04-13 13:06:30 +0100

    add translation support.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-pkg = 'Extensions.RemoteChannelStreamConverter'
-setup (name = 'enigma2-plugin-extensions-remotechannelstreamconverter',
-	version = '1.0',
-	description = 'Convert remote channel list for streaming',
-	packages = [pkg],
-	package_dir = {pkg: 'plugin'}
-)
---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+import os,gettext
+
+PluginLanguageDomain = "RemoteChannelStreamConverter"
+PluginLanguagePath = "Extensions/RemoteChannelStreamConverter/po"
+
+def localeInit():
+	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
+	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+	gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE, ""))
+
+def _(txt):
+	t = gettext.dgettext(PluginLanguageDomain, txt)
+	if t == txt:
+		t = gettext.dgettext('enigma2', txt)
+	return t
+
+
+localeInit()
+language.addCallback(localeInit)
