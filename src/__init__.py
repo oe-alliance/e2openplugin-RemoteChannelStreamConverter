a3289af29c1ae2010e31776a252a5e47bda4b040
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-11-29 15:19:34 +0000

    [Translation] update code.

--- src/__init__.py
+++ src/__init__.py
@@ -13,14 +13,13 @@ def localeInit():
 		lang = language.getLanguage()[:2]
 	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
-	gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE, ""))
 
 def _(txt):
 	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
-		t = gettext.dgettext('enigma2', txt)
+		print "[" + PluginLanguageDomain + "] fallback to default translation for", txt
+		t = gettext.gettext(txt)
 	return t
-
 
 localeInit()
 language.addCallback(localeInit)
